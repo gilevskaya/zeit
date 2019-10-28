e2b4d025e26aec6ef465ae8c5720db4530ef8c41
Alena Gilevskaya <alena@gilevskaya.com>
2019-10-27 20:59:42 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Zeit playground</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +21,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-};
+}
 
 export default App;
